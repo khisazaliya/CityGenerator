5eac1ec401ed7b7cde6fa600a248474608840e35
Azaliya <lkventina@gmail.com>
2024-01-28 13:23:54 +0300

    + road generating

---
+++ CityGenerator/Assets/Scripts/AgentParameters.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentParameters : MonoBehaviour
+{
+    public Vector3 position, direction;
+    public int length;
+}
---
+++ CityGenerator/Assets/Scripts/StructureHelper.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StructureHelper : MonoBehaviour
+{
+    public float animationTime = 0.01f;
+    public float blockSubdivisionThreshold = 100f;
+
+}
