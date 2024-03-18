8f428ab889b8f3a853de22254905ba665a5c2744
Azaliya <lkventina@gmail.com>
2024-03-18 13:10:16 +0300

    bug fix

---
+++ CityGenerator/Assets/Scripts/WFC/CellData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[System.Serializable]
+public class CellData
+{
+    public Vector2 coords; // Координаты ячейки
+    public bool isCollapsed; // Флаг, указывающий на то, произошло ли свертывание ячейки
+    // Другие параметры ячейки...
+
+    public CellData(Vector2 coords, bool isCollapsed)
+    {
+        this.coords = coords;
+        this.isCollapsed = isCollapsed;
+        // Инициализация других параметров ячейки...
+    }
+}
---
+++ CityGenerator/Assets/Scripts/WFC/CellManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CellManager : MonoBehaviour
+{
+    public string cellDataFilePath; // Путь к файлу, где будет сохранена информация о ячейках
+
+    // Метод для сохранения информации о ячейках в файл
+    public void SaveCellData(List<CellData> cellDataList)
+    {
+        string jsonData = JsonUtility.ToJson(cellDataList); // Преобразование списка данных в формат JSON
+        File.WriteAllText(cellDataFilePath, jsonData); // Запись данных в файл
+        Debug.Log("Cell data saved.");
+    }
+
+    // Метод для загрузки информации о ячейках из файла
+    public List<CellData> LoadCellData()
+    {
+        if (File.Exists(cellDataFilePath))
+        {
+            string jsonData = File.ReadAllText(cellDataFilePath); // Чтение данных из файла
+            return JsonUtility.FromJson<List<CellData>>(jsonData); // Преобразование JSON в список объектов CellData
+        }
+        else
+        {
+            Debug.LogError("Cell data file not found: " + cellDataFilePath);
+            return new List<CellData>();
+        }
+    }
+}
